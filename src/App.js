463e601ccd4ba197c2d255749dc2030ceae49d33
kırıll <kirill@krlls-MacBook-Pro.local>
2022-08-01 15:48:32 +0300

    checking gh-actions pipeline

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SOME OTHER TEXT
         </a>
       </header>
     </div>
